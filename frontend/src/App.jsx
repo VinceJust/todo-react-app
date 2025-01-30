e61fc96997fd828d48984fc3d46929a7f27b7f38
VinJust <vpj97@outlook.de>
2025-01-30 15:03:27 +0100

    Created simple but functioning index.html, main.jsx and App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Meine To-Do-App</h1>
+      { /* Hier kommt der Rest der App hin */ }
+    </div>
+  );
+}
+
+export default App;
